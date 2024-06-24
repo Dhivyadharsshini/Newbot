179e100c67dfcd8a2a72b527e55131ce3e8d652b
Dhivyadharsshini <166577460+Dhivyadharsshini@users.noreply.github.com>
2024-06-24 22:26:34 +0530

    getLast.method

---
+++ getLast.java
@@ -0,0 +1,14 @@
+import java.util.LinkedList;
+public class getLast {
+    
+public static void main(String args[])
+{
+    LinkedList<String> bikes=new LinkedList<>();
+    bikes.add("Pulsar");
+    bikes.add("Rx100");
+    bikes.add("Enfield");
+    System.out.println(bikes.getLast());
+
+}
+}
+
