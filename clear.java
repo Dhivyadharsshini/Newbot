2a2c4181e9fbc64c1bc834c059d6db822a25dae4
Dhivyadharsshini <166577460+Dhivyadharsshini@users.noreply.github.com>
2024-06-24 22:43:36 +0530

    clear.method

---
+++ clear.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class clear {
+    public static void main(String args[])
+{
+    LinkedList<String> fruits=new LinkedList<>();
+    fruits.add("Apple");
+    fruits.add("Papaya");
+    fruits.add("Banana");
+    System.out.println(fruits);
+    fruits.clear();
+    System.out.println(fruits);
+}
+}
+
+    
+
