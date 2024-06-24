4ef1519cc93e895011ce94bc37120dfc9849ca13
Dhivyadharsshini <166577460+Dhivyadharsshini@users.noreply.github.com>
2024-06-24 18:43:59 +0530

    list

---
+++ linkedlist.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+class simpleprogram{
+    public static void main(String args[])
+    {
+        LinkedList<String> colors=new LinkedList<>();
+        colors.add("Black");
+        colors.add("Red");
+        colors.add("Blue");
+        System.out.println(colors);
+    }
+}
