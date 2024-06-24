eb9cde808fd1df601cc626f3b1d5a71a5a9c4600
Dhivyadharsshini <166577460+Dhivyadharsshini@users.noreply.github.com>
2024-06-24 23:31:31 +0530

    size.method

---
+++ size.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class size {
+    public static void main(String args[])
+{
+    LinkedList<String> shampoo=new LinkedList<>();
+    shampoo.add("dove");
+    shampoo.add("clinicplus");
+    shampoo.add("sunsilk");
+    shampoo.add("pantene");
+    shampoo.add("karthika");
+    System.out.println(shampoo.size());
+    
+}
+}
