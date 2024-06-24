83ce77a2db98e2b949a123f0bbfaa36ae829752e
Dhivyadharsshini <166577460+Dhivyadharsshini@users.noreply.github.com>
2024-06-24 22:55:09 +0530

    removeFirst.java

---
+++ removeFirst.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+
+public class removeFirst {public static void main(String args[])
+{
+    LinkedList<String> icecream=new LinkedList<>();
+    icecream.add("Vannila");
+    icecream.add("Chocolate");
+    icecream.add("ButterScotch");
+    icecream.removeFirst();
+    
+    System.out.println(icecream);
+    
+}
+
+    
+}
