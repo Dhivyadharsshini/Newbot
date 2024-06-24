53aef27e583f188b4e8d7863182f383f1fecb246
Dhivyadharsshini <166577460+Dhivyadharsshini@users.noreply.github.com>
2024-06-24 22:19:06 +0530

    getFirst.method

---
+++ getFirst.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class getFirst {
+    public static void main(String args[])
+    {
+        LinkedList<String> colors=new LinkedList<>();
+        colors.add("Black");
+        colors.add("Red");
+        colors.add("Blue");
+        System.out.println(colors.getFirst());
+
+    }
+}
+
+
