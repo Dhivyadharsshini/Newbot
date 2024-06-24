6c37fae80c246d671be7e24a808505c87d645ae9
Dhivyadharsshini <166577460+Dhivyadharsshini@users.noreply.github.com>
2024-06-24 22:02:04 +0530

    addFirst.method

---
+++ addFirst.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class addFirst {
+
+    public static void main(String args[])
+    {
+        LinkedList<String> colors=new LinkedList<>();
+        colors.add("Black");
+        colors.add("Red");
+        colors.addFirst("Blue");
+        
+        System.out.println(colors);
+
+    }
+}
