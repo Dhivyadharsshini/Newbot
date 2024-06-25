a6865d93ce8af1dfe44db1de2ce46999739b4d7f
Dhivyadharsshini <166577460+Dhivyadharsshini@users.noreply.github.com>
2024-06-25 14:23:40 +0530

    set.method

---
+++ set.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class set {
+    public static void main(String args[])
+{
+    LinkedList<String> fruits =new LinkedList<>();
+    fruits.add("strawberry");
+    fruits.add("papaya");
+    fruits.add("orange");
+    fruits.add("banana");
+    fruits.set(2,"apple");
+    System.out.println(fruits);
+    
+}
+}
