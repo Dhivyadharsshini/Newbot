7244c009420d176971e1a243eeea69922a9e3008
Dhivyadharsshini <166577460+Dhivyadharsshini@users.noreply.github.com>
2024-06-25 14:53:16 +0530

    reverseLinkedList

---
+++ reverse.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class reverse {
+    public static void main(String[] args){
+        LinkedList<String> name=new LinkedList<>();
+        
+         name.add("Dhivya");
+         name.add("Dharshini");
+         name.add("Bharathi");
+         System.out.print(name.reversed());
+    }
+    
+}
