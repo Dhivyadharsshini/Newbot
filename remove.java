5aa2e5a39daa522a9d8c8654eb1a71ebcbd8cdf8
Dhivyadharsshini <166577460+Dhivyadharsshini@users.noreply.github.com>
2024-06-25 14:32:36 +0530

    removeFirstOccurance

---
+++ remove.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+
+public class remove {
+    public static void main(String args[])
+{
+    LinkedList<String> bikes=new LinkedList<>();
+    bikes.add("Pulsar");
+    bikes.add("Rx100");
+    bikes.add("Enfield");
+    bikes.add("Rx100");
+    bikes.removeFirstOccurrence("Rx100");
+    System.out.println(bikes);
+
+}
+}
+
+
