ca8daf2d8e824e0cf0dd03ee302a1eaf5e7eb1c0
Dhivyadharsshini <166577460+Dhivyadharsshini@users.noreply.github.com>
2024-07-02 12:50:50 +0530

    default constructor

---
+++ default.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Demo{
+    int x;
+    
+    void display(){
+        System.out.println(x);
+    }
+}
+class Example{
+    public static void main(String args[]){
+     Demo d=new Demo();
+     d.display();
+    }
+}
+    
+
