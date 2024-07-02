0fec3c4087fb7911d66f0ead8dcf3c8cff84db41
Dhivyadharsshini <166577460+Dhivyadharsshini@users.noreply.github.com>
2024-07-02 12:48:34 +0530

    Zero constructor

---
+++ zero.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+class Demo {
+  String name;
+  int marks;
+  Demo(){
+    name="Dhivya";
+    marks=99;
+  }
+void display(){
+    System.out.println(name);
+    System.out.println(marks);
+}
+}
+public class zero{
+    public static void main(String args[]){
+        Demo d=new Demo();
+        d.display();
+    }
+}
+
+
