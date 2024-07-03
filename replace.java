8c8c921f217ad2da7c4de7ab77ee5397470e1902
Dhivyadharsshini <166577460+Dhivyadharsshini@users.noreply.github.com>
2024-07-03 12:34:43 +0530

    replace

---
+++ replace.java
@@ -0,0 +1,12 @@
+
+    import java.util.*;
+public class replace{
+    public static void main(String args[]){
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter the String:");
+     String str=sc.nextLine();
+     String str1=str.replaceAll("[AEIOUaeiou]"," ");
+     System.out.println(str1);
+}
+}
+
